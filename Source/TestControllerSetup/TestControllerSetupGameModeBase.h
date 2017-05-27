71798e6aec5c52ba3b0e09f4affa3babbd8c675e
sky_b <sky_b@bitbucket.org>
2017-05-27 15:19:15 +0200

    add initial commit

---
+++ Source/TestControllerSetup/TestControllerSetupGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TestControllerSetupGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTCONTROLLERSETUP_API ATestControllerSetupGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
